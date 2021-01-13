cfe6b9ec10cadbd36f5f7c32b209319c26ef1c2d
Sergio <a2019112566@estudiantes.upsa.edu.bo>
2021-01-12 21:42:20 -0400

    SegundoCommit

--- PrimerConsola/PrimerConsola/Program.cs
+++ PrimerConsola/PrimerConsola/Program.cs
@@ -6,7 +6,7 @@ namespace PrimerConsola
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Buenas noches, Sergio");
         }
     }
 }
